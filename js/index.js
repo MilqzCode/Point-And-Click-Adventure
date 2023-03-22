e3b9ba6b177ddda035fd03351a4efcd69f7b36c9
Milqz <milan.rahmie@sintlucasedu.nl>
2023-03-22 12:53:55 +0100

    4

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
-document.getElementById("mainTitle").innerText = "Point and Click adventure."
-document.getElementById("mainTitle").style.textAlign = "center"
+document.getElementById("mainTitle").innerText = "Point and Click adventure";
+document.getElementById("mainTitle").style.textAlign = "center";
+
+const offsetCharacter = 16;
+
+const mainCharacter = document.getElementById("mainCharacter");
+const gameWindow = document.getElementById("gameWindow");
+
+
+gameWindow.onclick =  function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top; //y position within the element.
+    mainCharacter.style.left = x -offsetCharacter + "px";
+    mainCharacter.style.top = y-offsetCharacter + "px";
+
+    console.log(e.target.id);
+
+    switch(e.target.id){
+        case "door1":
+            //something here
+            break;
+        case "door2":
+            //something insert here
+            break;
+        default:
+            break;
+    }
+    //console.log(x);
+
+    //put in css on main character "Transition: all us ease-in-out;
+}
